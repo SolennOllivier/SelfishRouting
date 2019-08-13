88529c2f516816b57b95a1464ef1cb6079b47f7b
Solenn <solenn.ollivier@ensam.eu>
2019-08-13 13:47:43 -0700

    chapter1

---
+++ assets/js/AccordionChapters.js
@@ -0,0 +1,48 @@
+document.write('<div class="ui styled fluid accordion">\
+                                <div class="title">\
+                                    <i class="dropdown icon"></i>The App-induced deterioration of urban road mobility </div>\
+                                <div class="content">\
+                                    <p class="transition hidden">\
+                                        <a href="/SelfishRouting/Chapters/Chapt1.html"> The creation of thru-traffic chaos by apps  </a>\
+                                        <br>\
+                                        <a href="/SelfishRouting/Chapters/Chapt1-2.html"> The challenge of classical traffic control to contain thru-traffic </a>\
+                                        </p>\
+                                </div>\
+                                <div class="title">\
+                                    <i class="dropdown icon"></i> Extending traffic planning models to traffic control</div>\
+                                <div class="content active">\
+                                    <p class="transition hidden">\
+                                        <a href="#Chapt.html"> Traffic demand modeling for traffic control </a>\
+                                        <br>\
+                                        <a href="#chap.html"> Routing behavior modeling</a>\
+                                        <br>\
+                                        <a href="#chapt.html"> Dynamic traffic assignment</a>\
+                                        <br>\
+                                        <a href="#Chapt"> Simulators for traffic and land use</a>\
+                                        <br>\
+                                        <a href="#chap.html"> Traffic nowcast (estimation) and forecast (prediction)</a>\
+                                        <br>\
+                                        <a href="#chap.html"> Empty Chapt?</a>\
+                                        <br>\
+                                        <a href="#chap.html"> Impact analysis</a>\
+                                        <br>\
+                                        <a href="#chap.html">Policies to solve the Waze problem</a>\
+                                        <br>\
+                                        <a href="#chap.html"> Pricing policies</a>\
+                                        <br>\
+                                        <a href="#chap.html"> Cyber-physical Systems-enhanced Traffic Management: A proof of Concept</a>\
+                                        <br>\
+                                        </p>\
+                                </div>\
+                                <div class="title">\
+                                    <i class="dropdown icon"></i> The way forward and recommendations </div>\
+                                <div class="content">\
+                                    <p class="transition hidden">\
+                                        <a href="#chapt.html"> Toward metropolitan scale management traffic</a>\
+                                        <br>\
+                                        <a href="#hpat.html"> Recommendations </a>\
+                                        <br>\
+                                        <a href="#chap.html">Conclusion and Future</a>\
+                                        </p>\
+                                </div>\
+                            </div>');
