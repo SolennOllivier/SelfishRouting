37246ae6b2efd1bc1cd5fc9c14d007c73701d5f8
Solenn <solenn.ollivier@ensam.eu>
2019-08-21 15:51:19 -0700

    links update

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,14 +3,14 @@ document.write('<footer class="background-1 color-8 py-3 fs--1 fw-600 font-headi
                         <div class="row align-items-center">\
                             <div class="col-auto font-1">&copy; 2019 Mobile Sensing Lab</div>\
                             <div class="col fs-0 text-right">\
-                                <a class="p-2 color-8"  target="_blank" href="https://twitter.com/BerkeleyMsl">\
+                                <a class="p-2 color-8"  target="_blank" href="https://twitter.com/SelfishRouting">\
                                     <i class="fa fa-twitter"></i>\
                                 </a>\
-                                <a class="p-2 color-8"  target="_blank" href="https://www.facebook.com/ITSBerkeley/">\
+                                <a class="p-2 color-8"  target="_blank" href="https://www.facebook.com/SelfishRouting/">\
                                     <i class="fa fa-facebook"></i>\
                                 </a>\
                                 </a>\
-                                <a class="p-2 color-8"  target="_blank" href="mailto:selfishrouting@gmail.com">\
+                                <a class="p-2 color-8"  target="_blank" href="/SelfishRouting/Contact.html">\
                                     <i class="fa fa-envelope"></i>\
                                 </a>\
                             </div>\
